6130ec408c6518d343988700f22e0e79fe403551
Adilet Baimyrza Uulu <a.baimyrza@godeltech.com>
2024-08-19 18:59:46 +0200

    add FooterBar component

---
+++ src/components/FooterBar/FooterBar.vue
@@ -0,0 +1,20 @@
+<template>
+  <footer :class="$style['container']">drag and drop to reorder list</footer>
+</template>
+
+<script>
+export default {
+  name: 'FooterBar',
+}
+</script>
+
+<style lang="scss" module>
+.container {
+  margin-top: 4em;
+  color: #b1b1b1;
+
+  &::first-letter {
+    text-transform: capitalize;
+  }
+}
+</style>
