caf23fb281acbc2c8795d852ac375d37f7349c9d
Adilet Baimyrza Uulu <a.baimyrza@godeltech.com>
2024-08-19 18:38:01 +0200

    add CoreIcon component

---
+++ src/components/CoreIcon/CoreIcon.vue
@@ -0,0 +1,35 @@
+<template>
+  <img
+    :class="$style['icon']"
+    :src="src"
+    :width="width"
+    :height="height"
+    alt="Icon"
+  />
+</template>
+
+<script>
+export default {
+  name: 'CoreIcon',
+  props: {
+    src: {
+      type: String,
+      required: true,
+    },
+    width: {
+      type: String,
+      default: '24px',
+    },
+    height: {
+      type: String,
+      default: '24px',
+    },
+  },
+}
+</script>
+
+<style lang="scss" module>
+.icon {
+  display: block;
+}
+</style>
