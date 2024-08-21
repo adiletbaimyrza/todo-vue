64b25b9eda512b0f7db669f5f421a21b921633ca
Adilet Baimyrza Uulu <a.baimyrza@godeltech.com>
2024-08-21 09:18:43 +0200

    use more meaningful names

--- src/store/index.js
+++ src/store/index.js
@@ -20,24 +20,26 @@ export default createStore({
       state.todos.push(todo)
     },
     check(state, todoId) {
-      const todo = state.todos.find(t => t.id === todoId)
+      const todo = state.todos.find(todo => todo.id === todoId)
       if (todo) {
         todo.isCompleted = !todo.isCompleted
       }
     },
     delete(state, todoId) {
-      state.todos = state.todos.filter(t => t.id !== todoId)
+      state.todos = state.todos.filter(todo => todo.id !== todoId)
     },
     setFilter(state, filter) {
       state.currentFilter = filter
     },
     deleteCompleted(state) {
-      state.todos = state.todos.filter(t => !t.isCompleted)
+      state.todos = state.todos.filter(todo => !todo.isCompleted)
     },
   },
   getters: {
     sortedTodos(state) {
-      return state.todos.slice().sort((a, b) => a.isCompleted - b.isCompleted)
+      return state.todos
+        .slice()
+        .sort((todoA, todoB) => todoA.isCompleted - todoB.isCompleted)
     },
     filteredTodos(state) {
       let filtered = []
@@ -46,18 +48,20 @@ export default createStore({
           filtered = state.todos
           break
         case TodoFilter.ACTIVE:
-          filtered = state.todos.filter(t => !t.isCompleted)
+          filtered = state.todos.filter(todo => !todo.isCompleted)
           break
         case TodoFilter.COMPLETED:
-          filtered = state.todos.filter(t => t.isCompleted)
+          filtered = state.todos.filter(todo => todo.isCompleted)
           break
         default:
           filtered = state.todos
       }
-      return filtered.sort((a, b) => a.isCompleted - b.isCompleted)
+      return filtered.sort(
+        (todoA, todoB) => todoA.isCompleted - todoB.isCompleted
+      )
     },
     activeCount(state) {
-      return state.todos.filter(t => !t.isCompleted).length
+      return state.todos.filter(todo => !todo.isCompleted).length
     },
   },
 })
