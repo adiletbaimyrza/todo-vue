6210ba1b27239f7a49c919dc5d0fd55e24af385a
Adilet Baimyrza Uulu <a.baimyrza@godeltech.com>
2024-08-21 10:59:37 +0200

    add Theme enum

--- src/constants.js
+++ src/constants.js
@@ -3,3 +3,9 @@ export const TodoFilter = Object.freeze({
   ACTIVE: 'active',
   COMPLETED: 'completed',
 })
+
+export const Theme = Object.freeze({
+  TO_STRING: 'theme',
+  LIGHT: 'light',
+  DARK: 'dark',
+})
