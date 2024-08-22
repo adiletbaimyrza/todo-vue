bcbb294682860cc8bfdb4c7c39faa1f0a7eb44cc
Adilet Baimyrza Uulu <a.baimyrza@godeltech.com>
2024-08-22 10:05:27 +0200

    add tests for CoreIcon

---
+++ src/components/CoreIcon/CoreIcon.spec.js
@@ -0,0 +1,39 @@
+import { shallowMount } from '@vue/test-utils'
+import CoreIcon from './CoreIcon.vue'
+
+/* global describe it expect */
+describe('CoreIcon.vue', () => {
+  it('renders correctly', () => {
+    const wrapper = shallowMount(CoreIcon, {
+      propsData: {
+        src: 'test-icon.png',
+      },
+    })
+    expect(wrapper.exists()).toBe(true)
+  })
+
+  it('renders with correct src, width, and height props', () => {
+    const wrapper = shallowMount(CoreIcon, {
+      propsData: {
+        src: 'test-icon.png',
+        width: '32px',
+        height: '32px',
+      },
+    })
+    const img = wrapper.find('img')
+    expect(img.attributes('src')).toBe('test-icon.png')
+    expect(img.attributes('width')).toBe('32px')
+    expect(img.attributes('height')).toBe('32px')
+  })
+
+  it('applies default width and height when not provided', () => {
+    const wrapper = shallowMount(CoreIcon, {
+      propsData: {
+        src: 'test-icon.png',
+      },
+    })
+    const img = wrapper.find('img')
+    expect(img.attributes('width')).toBe('24px')
+    expect(img.attributes('height')).toBe('24px')
+  })
+})
