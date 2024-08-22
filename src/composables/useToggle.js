7c19970c9bdbe4051c3df97f647cb9a85aa79565
Adilet Baimyrza Uulu <a.baimyrza@godeltech.com>
2024-08-22 13:34:37 +0200

    fix useToggle

--- src/composables/useToggle.js
+++ src/composables/useToggle.js
@@ -1,11 +1,18 @@
 import { ref } from 'vue'
 
-const useToggle = (targetRef = ref(false)) => {
+const useToggle = targetRef => {
+  if (!targetRef) {
+    targetRef = ref(false)
+  }
+
   if (typeof targetRef !== 'object' || !('value' in targetRef)) {
     throw new TypeError('targetRef must be a ref object')
   }
+
   return () => {
     targetRef.value = !targetRef.value
+
+    return targetRef.value
   }
 }
 
