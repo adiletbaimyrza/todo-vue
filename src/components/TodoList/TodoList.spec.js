0f82d5499de45df73883dddd2fe1252d4e5ede9f
Adilet Baimyrza Uulu <a.baimyrza@godeltech.com>
2024-08-25 18:13:31 +0200

    add test to check drag and drop functionality

--- src/components/TodoList/TodoList.spec.js
+++ src/components/TodoList/TodoList.spec.js
@@ -4,18 +4,21 @@ import TodoList from './TodoList.vue'
 import TodoItem from '../TodoItem/TodoItem.vue'
 import FilterBar from '../FilterBar/FilterBar.vue'
 
-/* global describe beforeEach expect it */
+/* global describe beforeEach expect it jest */
 describe('TodoList.vue', () => {
   let store
   let state
   let getters
   let wrapper
+  let commitSpy
 
   beforeEach(() => {
     state = {
       todos: [
         { id: 1, content: 'Task 1', isCompleted: false },
-        { id: 2, content: 'Task 2', isCompleted: true },
+        { id: 2, content: 'Task 2', isCompleted: false },
+        { id: 3, content: 'Task 3', isCompleted: false },
+        { id: 4, content: 'Task 4', isCompleted: false },
       ],
     }
 
@@ -33,6 +36,8 @@ describe('TodoList.vue', () => {
         plugins: [store],
       },
     })
+
+    commitSpy = jest.spyOn(store, 'commit')
   })
 
   it('renders correctly with initial state', () => {
@@ -56,5 +61,25 @@ describe('TodoList.vue', () => {
   it('renders the FilterBar component', () => {
     const filterBar = wrapper.findComponent(FilterBar)
     expect(filterBar.exists()).toBe(true)
+  })
+
+  it('should reorder items on drag and drop', async () => {
+    await wrapper.vm.onDragStart(0)
+    expect(wrapper.vm.draggedItemIndex).toBe(0)
+
+    const event = { preventDefault: jest.fn() }
+    await wrapper.vm.onDragOver(event, 1)
+    expect(wrapper.vm.filteredTodos[0].id).toBe(2)
+    expect(wrapper.vm.filteredTodos[1].id).toBe(1)
+    expect(wrapper.vm.filteredTodos[2].id).toBe(3)
+
+    await wrapper.vm.onDrop()
+    expect(wrapper.vm.draggedItemIndex).toBe(null)
+    expect(commitSpy).toHaveBeenCalledWith('updateOrder', [
+      { id: 2, content: 'Task 2', isCompleted: false },
+      { id: 1, content: 'Task 1', isCompleted: false },
+      { id: 3, content: 'Task 3', isCompleted: false },
+      { id: 4, content: 'Task 4', isCompleted: false },
+    ])
   })
 })
